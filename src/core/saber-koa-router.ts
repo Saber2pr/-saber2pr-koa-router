d84c4357c5936bcdbe6c75a92ef22457f2b258f4
saber2pr <saber2pr@gmail.com>
2019-04-30 19:07:27 +0000

    update

--- src/core/saber-koa-router.ts
+++ src/core/saber-koa-router.ts
@@ -6,9 +6,14 @@
  */
 import { Job } from '@saber2pr/koa'
 
-export function route(path: string) {
+export type Rule = (requestUrl: string, path: string) => boolean
+
+export function route(
+  path: string,
+  rule: Rule = (requestUrl, path) => requestUrl === path
+) {
   return <T>(job: Job<T>): Job<T> => async (ctx, next) => {
-    if (ctx.request.url === path) {
+    if (rule(ctx.request.url, path)) {
       await job(ctx, next)
     } else {
       await next()
@@ -26,17 +31,24 @@ export function method(type: 'GET' | 'POST' | 'PUT' | 'DELETE') {
   }
 }
 
+const MATCH: Record<'GET' | 'POST' | 'DEL' | 'PUT', Rule> = {
+  DEL: (req, path) => req.split('?')[0] === path,
+  GET: (req, path) => req.split('?')[0] === path,
+  POST: (req, path) => req.split('?')[0] === path,
+  PUT: (req, path) => req.split('?')[0] === path
+}
+
 export namespace route {
-  export function get(path: string) {
-    return <T>(job: Job<T>): Job<T> => route(path)(method('GET')(job))
+  export function get(path: string, rule: Rule = MATCH.GET) {
+    return <T>(job: Job<T>): Job<T> => route(path, rule)(method('GET')(job))
   }
-  export function post(path: string) {
-    return <T>(job: Job<T>): Job<T> => route(path)(method('POST')(job))
+  export function post(path: string, rule: Rule = MATCH.POST) {
+    return <T>(job: Job<T>): Job<T> => route(path, rule)(method('POST')(job))
   }
-  export function del(path: string) {
-    return <T>(job: Job<T>): Job<T> => route(path)(method('DELETE')(job))
+  export function del(path: string, rule: Rule = MATCH.DEL) {
+    return <T>(job: Job<T>): Job<T> => route(path, rule)(method('DELETE')(job))
   }
-  export function put(path: string) {
-    return <T>(job: Job<T>): Job<T> => route(path)(method('PUT')(job))
+  export function put(path: string, rule: Rule = MATCH.PUT) {
+    return <T>(job: Job<T>): Job<T> => route(path, rule)(method('PUT')(job))
   }
 }
--- src/test/request.ts
+++ src/test/request.ts
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
-axios.get('http://localhost:3004/get').then(res => console.log(res.data))
-axios.post('http://localhost:3004/post').then(res => console.log(res.data))
-axios.put('http://localhost:3004/put').then(res => console.log(res.data))
-axios.delete('http://localhost:3004/del').then(res => console.log(res.data))
+axios
+  .get('http://localhost:3004/get?name=233')
+  .then(res => console.log(res.data))
+axios
+  .post('http://localhost:3004/post?name=233')
+  .then(res => console.log(res.data))
+axios
+  .put('http://localhost:3004/put?name=233')
+  .then(res => console.log(res.data))
+axios
+  .delete('http://localhost:3004/del?name=233')
+  .then(res => console.log(res.data))
